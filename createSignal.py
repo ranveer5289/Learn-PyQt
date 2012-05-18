8767a76c3ad1ac6123857a5328c9954eeb1c7c00
ranveer5289 <ranveer.raghu@gmail.com>
2012-05-18 20:28:52 +0530

    Create own custom signal

---
+++ createSignal.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class TaxRate(QObject):
+
+
+       def __init__(self):
+                super(TaxRate,self).__init__()
+                self.__rate = 17.5
+
+
+       def rate(self):
+               return self.__rate
+
+       def setRate(self,rate):
+               if rate != self.__rate:
+                       self.__rate = rate
+                       self.emit(SIGNAL("rateChanged"), self.__rate)
+
+
+
+def rateChanged(value):
+                print "value changed {0}".format(value)
+
+app = QApplication(sys.argv)
+vat = TaxRate()
+vat.connect(vat,SIGNAL("rateChanged"),rateChanged)
+vat.setRate(17.5)
+vat.setRate(7.5)
+vat.setRate(117.5)
+app.exec_()
